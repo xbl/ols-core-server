913bd12239d078fd7777b4749653c9d1e951fd00
YuanShenjian <sjyuan@thoughtworks.com>
2018-06-15 16:42:24 +0800

    Fix deploy script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-
-docker run --rm -v $(pwd):/app -w /app frekele/gradle:3.5.1-jdk8 /bin/bash -c "./gradlew clean build -x test"
+./gradlew clean build -x test
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm -v $(pwd):/app -w /app frekele/gradle:3.5.1-jdk8 /bin/bash -c "gradle clean test"
+./gradlew clean test
