2df05b66d7f604bff0ffb389a7918941afe58ffd
YuanShenjian <sjyuan@thoughtworks.com>
2018-06-15 15:32:22 +0800

    Fix deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker rm -f $(docker ps -a | grep user-service | awk '{print $1}') || echo 'Not existed'
-docker run -it  --name user-service -p 8081:8081 -v /var/jenkins_home/workspace/ols-core-server-pipeline:/app -w /app java:8 /bin/bash
+docker run --name user-service -p 8081:8081 -v $(pwd):/app -w /app java:8 /bin/bash -c "java -jar /app/build/libs/ols-core-server-0.0.1-SNAPSHOT.jar"
 
