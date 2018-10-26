133fc0c238251929d9288066f39a29302a578e7d
xbl <blxie@toughtworks.com>
2018-10-26 18:20:40 +0800

    text: update text

--- src/main/java/com/thoughtworks/nho/api/GreetingController.java
+++ src/main/java/com/thoughtworks/nho/api/GreetingController.java
@@ -11,6 +11,6 @@ public class GreetingController {
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public String greet() {
-        return "Hello, Spring MVC,111";
+        return "Hello, Spring MVC";
     }
 }
