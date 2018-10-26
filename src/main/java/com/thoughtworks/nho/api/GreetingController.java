28b2c679b807470f4e10454a59958830d84580cd
Yuan Shenjian <sjyuan.cc@gmail.com>
2018-10-26 17:56:08 +0800

    Change for test
    Signed-off-by: Yuan Shenjian <sjyuan.cc@gmail.com>

--- src/main/java/com/thoughtworks/nho/api/GreetingController.java
+++ src/main/java/com/thoughtworks/nho/api/GreetingController.java
@@ -11,6 +11,6 @@ public class GreetingController {
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public String greet() {
-        return "Hello, Spring MVC";
+        return "Hello, Spring MVC11111";
     }
 }
